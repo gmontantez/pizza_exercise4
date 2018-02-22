00a4788a57fa66d244f47711d550e393db9d29d2
gmontantez <gmontantez7@gmail.com>
2018-02-21 21:09:42 -0500

    Final Pizza Exercise

--- pizza_exercise4.rb
+++ pizza_exercise4.rb
@@ -1,28 +1,62 @@
-menu_title = ["\nCatalona's Pizza"]
-menu = ["\n\nPizza Sizes\nSmall $13.00  Medium $16.00  Large $16.00"]
-menu_crust = ["\nCrust Flavors\nPlain  Garlic  Stuffed"]
-menu_sauces = ["\nSauces\nTomato  Alfredo"]
-menu_toppings = ["\nToppings\nPepperoni  Sausage  Ham  Mushrooms  Black Olive  Green Pepper  Chicken  Spinach  Anchovies"]
-menu_extra_toppings = ["\nExtra Toppings\nExtra Cheese $1.00  Double Pepperoni $2.00  Feta Cheese $3.00"]
-menu_wings = ["\nWings\nBBQ  Honey Mustard  Garlic  Hot\n8 Piece $6.00  16 Piece $11.00  24 Piece $16.00"]
-menu_salad = ["\nSalads\nHouse Salad  Ceaser Salad  Greek Salad  $5.00 each\nSalad Dressing\nRanch  Blue Cheese  House  Ceaser  Greek"]
-menu_drinks = ["\nBottled Drinks\nCoke  Diet Coke  Sprite  Iced Tea  Water"]
-crust = ["plain","garlic","stuffed"]
-meats = ["pepperoni","sausage","ham","ground beef"]
-veggies = ["mushrooms","black olives","green peppers"]
-toppings = ["anchovies","chicken","spinach"]
-sauces = ["tomato","alfredo"]
-other_options = ["extra cheese","double pepperoni","feta cheese"] #special toppings costing extra
-wings = ["bbq","honey mustard","garlic","hot"]
-salad = ["house salad","ceaser salad","greek salad"]
-salad_dressings = ["ranch","blue cheese","house","ceaser","greek"]
-drinks = ["coke","diet coke","sprite","iced tea","water"]
-size = ["small","medium","large"]
-wings_size = ["8 piece","16 piece","24 piece"]
-wing_order = ["yes","no"]
-salad_order = ["yes","no"]
-drink_order = ["yes","no"]
-delivery = ["yes","no"]
+def menu 
+   "\nCatalona's Pizza
+   \n\nPizza Sizes\nSmall $13.00  Medium $16.00  Large $16.00
+   \nCrust Flavors\nPlain  Garlic  Stuffed
+   \nSauces\nTomato  Alfredo
+   \nToppings\nPepperoni  Sausage  Ham  Mushrooms  Black Olive  Green Pepper  Chicken  Spinach  Anchovies
+   \nExtra Toppings\nExtra Cheese $1.00  Double Pepperoni $2.00  Feta Cheese $3.00
+   \nWings\nBBQ  Honey Mustard  Garlic  Hot\n8 Piece $6.00  16 Piece $11.00  24 Piece $16.00
+   \nSalads\nHouse Salad  Ceaser Salad  Greek Salad  $5.00 each\nSalad Dressing\nRanch  Blue Cheese  House  Ceaser  Greek
+   \nBottled Drinks\nCoke  Diet Coke  Sprite  Iced Tea  Water"
+end
+def crust
+   crust = ["plain","garlic","stuffed"].sample
+end
+def meats
+   meats = ["pepperoni","sausage","ham","ground beef"].sample
+end
+def veggies
+   veggies = ["mushrooms","black olives","green peppers"].sample
+end
+def toppings
+   toppings = ["anchovies","chicken","spinach"].sample
+end
+def sauces
+   sauces = ["tomato","alfredo"].sample
+end
+def other_options
+   other_options = ["extra cheese","double pepperoni","feta cheese"].sample #special toppings costing extra
+end
+def wings
+   wings = ["bbq","honey mustard","garlic","hot"].sample
+end
+def salad
+   salad = ["house salad","ceaser salad","greek salad"].sample
+end
+def salad_dressings
+   salad_dressings = ["ranch","blue cheese","house","ceaser","greek"].sample
+end
+def drinks
+   drinks = ["coke","diet coke","sprite","iced tea","water"].sample
+end
+def size
+   size = ["small","medium","large"].sample
+end
+def wings_size
+   wings_size = ["8 piece","16 piece","24 piece"].sample
+end
+def wing_order
+   wing_order = ["yes","no"].sample
+end
+def salad_order
+   salad_order = ["yes","no"].sample
+end
+def drink_order
+   drink_order = ["yes","no"].sample
+end 
+def delivery
+   delivery = ["yes","no"].sample
+end
 
 
 def cost(size)
@@ -68,15 +102,8 @@ def delivery_choice(delivery)
    end
 end
 
-puts menu_title
 puts menu
-puts menu_crust
-puts menu_sauces
-puts menu_toppings
-puts menu_extra_toppings
-puts menu_wings
-puts menu_salad
-puts menu_drinks
+
 
 puts "\n\nHow many pizzas would you like?"
 pizzas = gets.chomp.to_i
@@ -89,13 +116,13 @@ puts "The special toppings are extra cheese, double pepperoni, and feta cheese."
 pizza = 1
 subtotal = 0
 while pizza <= pizzas do
-   pizza_size = size.sample
+   pizza_size = size
    pizza_cost = cost(pizza_size)
-   other = other_options.sample
+   other = other_options
    other_cost = special_options(other)
    puts "\nPizza #{pizza}"
    puts "So, you would like a #{pizza_size} pizza at $#{sprintf("%0.02f", pizza_cost)}."
-	puts "You would like a #{crust.sample} crust, with #{sauces.sample} sauce, your toppings will be #{veggies.sample}, #{meats.sample} and #{toppings.sample}."  
+	puts "You would like a #{crust} crust, with #{sauces} sauce, your toppings will be #{veggies}, #{meats} and #{toppings}."  
    puts "You would also like #{other} at $#{sprintf("%.02f", other_cost)}.\n"
    pizza +=1
 end
@@ -109,24 +136,24 @@ if additional_items_choice == "yes" || additional_items_choice == "Yes"
    puts "\nWould you like a drink?"
    drink_option = gets.chomp
    if wings_option == "yes"
-      wings_amount = wings_size.sample
+      wings_amount = wings_size
       wings_cost = amount(wings_amount)
       puts "\nSo, you would like #{wings_amount} for $#{sprintf("%.02f", wings_cost)}."
    end
    if salad_option == "yes"
-      salad_type = salad.sample
+      salad_type = salad
       salad_cost = 5.00
       puts "\nWould you like dressing with your salad?"
       dressing_choice = gets.chomp
       if dressing_choice == "yes"
-         dressing_type = salad_dressings.sample
-         puts "\nSo you would like #{salad_type} with #{dressing_type}."
+         dressing_type = salad_dressings
+         puts "\nSo you would like #{salad_type} with #{dressing_type} dressing."
       else
          puts "\nSo you would like #{salad_type}."
       end
    end
    if drink_option == "yes"
-      drink_type = drinks.sample
+      drink_type = drinks
       drink_cost = 1.75
       puts "\nSo you would like #{drink_type} for $#{sprintf("%.02f", drink_cost)}."
    end
@@ -142,7 +169,7 @@ subtotal += pizza_cost + other_cost + wings_cost + salad_cost + drink_cost
 total_cost = subtotal * tax
 
 
-delivery_option = delivery.sample
+delivery_option = delivery
 delivery_cost = delivery_choice(delivery_option)
 
 if delivery_option == "yes"
@@ -156,3 +183,4 @@ puts "\nThe tip amount is $#{sprintf("%.02f", tip)}."
 puts "\nThe total cost including tax is $#{sprintf("%.02f", total_cost)}."
 puts "\nYour choice for delivery is #{delivery_option}."
 puts "\nThe delivery cost is $#{sprintf("%.02f", delivery_cost)}."
+ 
